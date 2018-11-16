4ef1e5f71649c76cb83be7bae1787b65e7856954
James <jdenderplays@gmail.com>
2018-11-15 20:53:15 -0500

    Add a hello world App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    
+});
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+}
+</style>
