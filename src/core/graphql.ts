79433b09a4ca17e50286977808685c8adbc683fe
James <jdenderplays@gmail.com>
2018-11-15 21:34:15 -0500

    Add and connect all the links in graphql::createClient

--- src/core/graphql.ts
+++ src/core/graphql.ts
@@ -2,19 +2,39 @@ import ApolloClient from 'apollo-client';
 
 import { WebSocketLink } from 'apollo-link-ws';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { withClientState } from 'apollo-link-state';
 
-import { split } from 'apollo-link';
+import { split, ApolloLink, Observable  } from 'apollo-link';
 import { getMainDefinition } from 'apollo-utilities';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 interface CreateClientOptions {
-  uri: string;
+    uri: string;
+    token: string;
 }
 
-export const createClient = ({ uri }: CreateClientOptions) => {
+export const createClient = ({ uri, token }: CreateClientOptions) => {
 
     const { httpUrl, wsUrl } = createUrls(uri);
 
+    // TODO: Fix lint errors
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+        // tslint:disable 
+        if (graphQLErrors)
+            graphQLErrors.map(({ message, locations, path }) =>
+                console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                ),
+            );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+        // tslint:enable
+    });
+
+    // TODO: Add client state
+    const stateLink = withClientState();
+
+    // Websocket for subscriptions
     const wsLink = new WebSocketLink({
         uri: wsUrl,
         options: {
@@ -22,12 +42,13 @@ export const createClient = ({ uri }: CreateClientOptions) => {
         },
     });
 
+    // Http for everything else
     const httpLink = new HttpLink({
         uri: httpUrl,
     });
 
-    const link = split(
-        // Split based on operation type
+    // Split between ws and http based on operation type
+    const networkLink = split(
         ({ query }) => {
             const { kind, operation } = getMainDefinition(query);
 
@@ -37,12 +58,21 @@ export const createClient = ({ uri }: CreateClientOptions) => {
         httpLink,
     );
 
+    // Concat all the links
+    const link = ApolloLink.from([
+        errorLink,
+        stateLink,
+        networkLink,
+    ]);
+
+    // Return a client
     return new ApolloClient({
         link,
         cache: new InMemoryCache(),
     });
 };
 
+// Bad url thing for now
 const createUrls = (base: string, secure = true) => ({
     httpUrl: `http${secure ? 's' : ''}://${base}`,
     wsUrl: `ws${secure ? 's' : ''}://${base}`,
